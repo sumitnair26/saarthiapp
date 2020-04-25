de45c386903df93f9497cbd66f6c9b0a997c78b2
sumitnair26 <sumit.nair26@gmail.com>
2020-04-26 04:38:05 +0530

    Front page design and Share in WhatsApp Option

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,51 +1,86 @@
 <head>
+    <!-- bootstrap.min.css -->
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
      <!-- jQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- bootstrap.min.js -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css">
+
+    <!-- whatsApp Share -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/whatsapp.css">
+    <script src="<?php echo base_url(); ?>assets/js/whatsappShare.js"></script>
+    <style>
+      .body{
+        font-family: freeserif;
+      }
+    </style>
 </head>
 
 <body>
 
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>Saarthi PDF Download </h1>
-  <h2><?php echo base_url(); ?></h2>
+  <h1>Saarthi Contest</h1>
+  <h1>सारथी कांटेस्ट</h1>
 </div>
 
 <div class="container">
   <div class="row">
     <div class="col-sm-4">
-      <h2>About Me</h2>
-      <h5>Photo of me:</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <h3>Some Links</h3>
-      <p>Lorem ipsum dolor sit ame.</p>
+      <h2>Contest</h2>
+      <p>Download Saarthi for free today !</p>
+      <h3>Contest Link's</h3>
+      <p>ALL Contest's</p>
       <ul class="nav nav-pills nav-stacked">
         <li class="active"><a href="#">Link 1</a></li>
         <li><a href="<?php echo base_url(); ?>home/contestPdf">Contest Pdf</a></li>
-        <li><a href="#">Link 3</a></li>
+        <li><a data-toggle="modal" data-target="#myModal">Share Contest</a></li>
       </ul>
       <hr class="hidden-sm hidden-md hidden-lg">
     </div>
     <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2>Saarthi</h2>
+      <h5>
+      Share/Download </h5>
+      <p>Click on Link to download or Share Contest Q&A</p>
       <br>
-      <h2>TITLE HEADING</h2>
+      <h2 style="font-family: freeserif;"><b>सारथी कांटेस्ट प्रश्न </b></h2>
       <h5>Title description, Sep 2, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>Click on link to download Saarthi App for free ( Hindi + English )</p>
+      <p><span><a href="http://bit.ly/SAARTHI_APP"><i class="fa fa-hand-o-right"></i> http://bit.ly/SAARTHI_APP <i class="fa fa-hand-o-left"></i></a></span></p>
     </div>
   </div>
 </div>
 
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Footer</p>
+  <span> <a href="https://github.com/sumitnair26/">&copy; sumitnair26</a></spam>
 </div>
 
+
 </body>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Share Contest</h4>
+        </div>
+        <div class="modal-body">
+          <p>
+          <a data-text="Your message goes here.." data-link="http://w3lessons.info" class="whatsapp w3_whatsapp_btn w3_whatsapp_btn_large">
+            <i class="fa fa-whatsapp" aria-hidden="true"></i> Share In WhatApp
+          </a>
+          <a data-text="Your message goes here.." data-link="http://w3lessons.info" class="whatsapp w3_whatsapp_btn w3_whatsapp_btn_large">Download PDF</a>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
